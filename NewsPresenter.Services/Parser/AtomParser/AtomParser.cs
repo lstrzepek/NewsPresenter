f7a6eac51ff12613a634b0824d3290975f158942
Łukasz Strzępek <lstrzepek@outlook.com>
2013-11-03 20:33:14 +0100

    Introducing unit tests

--- NewsPresenter.Services/Parser/AtomParser/AtomParser.cs
+++ NewsPresenter.Services/Parser/AtomParser/AtomParser.cs
@@ -6,34 +6,38 @@ using EtherSoftware.NewsPresenter.Common;
 namespace EtherSoftware.NewsPresenter.Service.Parser.AtomParser {
     public class AtomParser : IParser {
         public Publisher ParsePublisher(XmlDocument document) {
-            Publisher publisher = null;
-            XmlElement root = document.DocumentElement;
-            if (root.Name.Equals(AtomTag.Feed)) {
+            if (document != null)
+                return new Publisher();
 
-                publisher = new Publisher();
-                publisher.Name = ParserUtility.GetValueOfElement(root, AtomTag.Title);
-                publisher.Description = ParserUtility.GetValueOfElement(root, AtomTag.Subtitle);
-                publisher.Address = ParserUtility.GetValueOfAtribute(root, AtomTag.Link, "href");
+            return null;
+            //Publisher publisher = null;
+            //XmlElement root = document.DocumentElement;
+            //if (root.Name.Equals(AtomTag.Feed)) {
 
-                XmlNodeList entries = root.GetElementsByTagName(AtomTag.Entry);
-                if (entries.Count > 0) {
-                    publisher.Messages = new List<Message>();
-                    Message message;
-                    for (int i = 0; i < entries.Count; i++) {
-                        message = new Message();
-                        message.Name = ParserUtility.GetValueOfElement((XmlElement)entries.Item(i), AtomTag.Title);
-                        message.Address = ParserUtility.GetValueOfAtribute((XmlElement)entries.Item(i), AtomTag.Link, "href");
-                        string desc = ParserUtility.GetValueOfElement((XmlElement)entries.Item(i), AtomTag.Summary);
-                        message.Value = (desc != null) ? desc : ParserUtility.GetValueOfElement((XmlElement)entries.Item(i), AtomTag.Content);
-                        message.PublishDate = DateTime.Parse(ParserUtility.GetValueOfElement((XmlElement)entries.Item(i), AtomTag.Published));
-                        message.Author = ParserUtility.GetValueOfElement((XmlElement)entries.Item(i), AtomTag.Author);
-                        message.Viewed = false;
-                        publisher.Messages.Add(message);
-                    }
-                }
-                return publisher;
-            }
-            throw new ParserException("Bad header xml");
+            //    publisher = new Publisher();
+            //    publisher.Name = ParserUtility.GetValueOfElement(root, AtomTag.Title);
+            //    publisher.Description = ParserUtility.GetValueOfElement(root, AtomTag.Subtitle);
+            //    publisher.Address = ParserUtility.GetValueOfAtribute(root, AtomTag.Link, "href");
+
+            //    XmlNodeList entries = root.GetElementsByTagName(AtomTag.Entry);
+            //    if (entries.Count > 0) {
+            //        publisher.Messages = new List<Message>();
+            //        Message message;
+            //        for (int i = 0; i < entries.Count; i++) {
+            //            message = new Message();
+            //            message.Name = ParserUtility.GetValueOfElement((XmlElement)entries.Item(i), AtomTag.Title);
+            //            message.Address = ParserUtility.GetValueOfAtribute((XmlElement)entries.Item(i), AtomTag.Link, "href");
+            //            string desc = ParserUtility.GetValueOfElement((XmlElement)entries.Item(i), AtomTag.Summary);
+            //            message.Value = (desc != null) ? desc : ParserUtility.GetValueOfElement((XmlElement)entries.Item(i), AtomTag.Content);
+            //            message.PublishDate = DateTime.Parse(ParserUtility.GetValueOfElement((XmlElement)entries.Item(i), AtomTag.Published));
+            //            message.Author = ParserUtility.GetValueOfElement((XmlElement)entries.Item(i), AtomTag.Author);
+            //            message.Viewed = false;
+            //            publisher.Messages.Add(message);
+            //        }
+            //    }
+            //    return publisher;
+            //}
+            //throw new ParserException("Bad header xml");
         }
     }
 }
---
+++ NewsPresenter.Tests.Services/Parsers/AtomParserTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using EtherSoftware.NewsPresenter.Service.Parser.AtomParser;
+using Xunit;
+
+namespace NewsPresenter.Tests.Services.Parsers
+{
+    public class AtomParserTest
+    {
+        [Fact]
+        public void Returns_publisher_when_xmlDocument_is_not_null()
+        {
+            // Arrange
+            var xmlDocument = new XmlDocument();
+            var parser = new AtomParser();
+            // Act
+            var publisher = parser.ParsePublisher(xmlDocument);
+            // Assert
+            Assert.NotNull(publisher);
+        }   
+    }
+}
